e200b3d7d667421975a26ae554655e31254b926f
Consuelo Gomez <mconsuelo.gomezt@gmail.com>
2022-08-08 12:20:56 -0400

    se agrega readme y se optimiza apariencia.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
-//Smooth scrolling effect//
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-          window.location.hash = hash;
-        });
-      } 
-    });
-  });
+ $(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
 
-  //Tooltips//
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 200, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+ 
+ 
+ 
+ //Tooltips//
   $(function () {
     $('[data-bs-toggle="tooltip"]').tooltip()
   })
---
+++ readme.md
@@ -0,0 +1,8 @@
+Esta maqueta está diseñada para ser un blog de viajes, con transición smooth y con un carousel para ser más atractivo a nivel visual.
+Es totalmente responsive.
+<img src="assets/img/Captura de Pantalla 2022-08-08 a la(s) 10.30.20.png">
+<img src="assets/img/Captura de Pantalla 2022-08-08 a la(s) 10.30.31.png">
+<img src="assets/img/Captura de Pantalla 2022-08-08 a la(s) 11.13.54.png">
+<img src="assets/img/Captura de Pantalla 2022-08-08 a la(s) 11.21.26.png">
+<img src="assets/img/Captura de Pantalla 2022-08-08 a la(s) 11.18.48.png">
+<img src="assets/img/Captura de Pantalla 2022-08-08 a la(s) 11.15.08.png">
